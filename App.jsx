d451477c3638cfe757f39ea7092aa97005f68593
Charles <charles11998866@gmail.com>
2025-12-23 20:23:09 -0500

    Replace placeholder public key with actual EmailJS public key mOVgbw3TGdTMQcyTm

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import emailjs from '@emailjs/browser';
+import { useEffect } from 'react';
+
+// Initialize EmailJS with the public key
+emailjs.init('mOVgbw3TGdTMQcyTm');
+
+export default function App() {
+  return (
+    <div className="app">
+      {/* Your app content here */}
+    </div>
+  );
+}

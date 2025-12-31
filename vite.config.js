74dcea848a762c612ba41f1198abf1f8cb46ee60
Charles <charles11998866@gmail.com>
2025-12-30 22:52:25 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { useImperativeHandle } from 'react';
 
 export default defineConfig({
   plugins: [react()],
-})
+})          
